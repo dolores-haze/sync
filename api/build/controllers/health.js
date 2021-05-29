9130156dbef73dfd45b4fae5b6c24e6242422d29
dolores-haze <hsgroves+doloreshaze@gmail.com>
2021-05-29 01:36:01 -0400

    remove build from version control

--- api/build/controllers/health.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.health = void 0;
-var health = function (req, res) {
-    res.status(200).send({
-        message: 'OK'
-    });
-};
-exports.health = health;
--- api/build/middleware/requestLogger.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.requestLogger = void 0;
-var requestLogger = function (req, res, next) {
-    var timestamp = new Date();
-    console.log(timestamp.toLocaleString() + " | " + req.path);
-    next();
-};
-exports.requestLogger = requestLogger;
